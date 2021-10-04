bd5b2ffdeaab0194e2545dfa6b5001ec71c51268
Anurag Chaudhary <70976859+anurag-25@users.noreply.github.com>
2021-10-05 00:54:16 +0530

    Time: 221 ms (10.30%), Space: 49.8 MB (7.40%) - LeetHub

--- coin-change/coin-change.cpp
+++ coin-change/coin-change.cpp
@@ -1,5 +1,19 @@
 class Solution {
 public:
+    // int coinChange(vector<int>& coins, int amount) {
+    //     int n = coins.size();
+    //     vector <int> dp (amount+1, amount+1);   // since max number of coins can be = amount
+    //     dp[0] = 0;
+    //     for (int i=1; i<=amount; i++) {
+    //         for (int j=0; j<n; j++) {
+    //             if (coins[j] <= i) 
+    //                 dp[i] = min (dp[i], dp[i-coins[j]]+1);
+    //         }
+    //     }
+    //     if (dp[amount]>amount) return -1;
+    //     return dp[amount];
+    // }
+    
     int solve (vector<int>& coins, int amount, int n, vector <vector<int>>& dp) {
         if (amount == 0) return 0;
         if (n ==0) return INT_MAX-1;
