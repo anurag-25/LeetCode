eeaec7d6f3784024a505adf4f6dd15c2c94b7f7e
Anurag Chaudhary <70976859+anurag-25@users.noreply.github.com>
2021-11-25 10:26:50 +0530

    Time: 20 ms (55.49%), Space: 24.3 MB (33.13%) - LeetHub

--- kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -10,15 +10,54 @@
  * };
  */
 class Solution {
-    int solve (TreeNode* root, int &k) {
-        if (root==NULL) return -1;
-        int left = solve (root->left, k);
-        if (left !=-1 ) return left;
-        if (--k == 0) return root->val;
-        return solve (root->right, k);
-    }
+    
 public:
+    // int kthSmallest(TreeNode* root, int k) {
+    //     TreeNode* curr = root;
+    //     stack<TreeNode*> nodes;
+    //     while(!nodes.empty() || curr != nullptr) {
+    //         while(curr!=nullptr) {
+    //             nodes.push(curr);
+    //             curr = curr->left;
+    //         }
+    //         TreeNode* temp = nodes.top();
+    //         nodes.pop();
+    //         if(--k == 0) {
+    //             return temp->val;
+    //         }
+    //         curr = temp->right;
+    //     }
+    //     return -1;
+    // }
     int kthSmallest(TreeNode* root, int k) {
-        return solve (root, k);
+        stack<TreeNode*> inorder;
+        TreeNode* curr = root;
+        
+        while (!inorder.empty() || curr ) {
+            while (curr) {
+                inorder.push(curr);
+                curr = curr->left;
+            }
+            TreeNode* temp = inorder.top();
+            inorder.pop();
+            if (--k == 0) return temp->val;
+            curr = temp->right;
+            
+            
+        }
+       return -1;
     }
 };
+
+
+// int solve (TreeNode* root, int &k) {
+//     if (root==NULL) return -1;
+//     int left = solve (root->left, k);
+//     if (left !=-1 ) return left;
+//     if (--k == 0) return root->val;
+//     return solve (root->right, k);
+// }
+// public:
+//     int kthSmallest(TreeNode* root, int k) {
+//         return solve (root, k);
+//     }
